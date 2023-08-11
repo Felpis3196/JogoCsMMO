2dfc67ceb658fa606f513692fb85a279a73d1692
Felpis3196 <126421629+Felpis3196@users.noreply.github.com>
2023-08-11 08:30:38 -0300

    Add files via upload

---
+++ MMOCS/MMOSOLUCAO/MMOCS/Personagem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MMOCS
+{
+    internal class Personagem
+    {
+        public string Nome { get; set; }
+        public int Nivel { get; set; }
+        public int QI { get; set; }
+        public int Agilidade { get; set; }
+        public int Vida { get; set; }
+        public string Raca { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int ID { get; set; }
+        public void apresentar()
+        {
+            Console.WriteLine($"Me chamo {Nome} sou um(a) {Raca}");
+            Console.WriteLine($"Tenho estes atributos \n Vida: {Vida} \n Força: {Forca} \n Defesa : {Defesa}  \n Agilidade: {Agilidade} \n Nivel: {Nivel} " +
+                $"Com um overral de {(Vida + Forca + Defesa + Agilidade) / 4}"
+                );
+        }
+        public virtual void atacar(Personagem inimigo)
+        {
+            int dano = Math.Max(0, Forca - inimigo.Defesa);
+            inimigo.Vida -= dano;
+            Console.WriteLine($"{Nome} ataca {inimigo.Nome} e causa {dano} de dano.");
+        }
+        public void regenerarVida()
+        {
+            int quantidadeRegenerada = QI / 2;
+            Vida = Math.Min(100, Vida + quantidadeRegenerada);
+            Console.WriteLine($"{Nome} regenera {quantidadeRegenerada} de vida.");
+        }
+        public void subirNivel()
+        {
+            Nivel++;
+            QI += 10;
+            Forca += 5;
+            Defesa += 4;
+            Console.WriteLine($"{Nome} subiu para o nível {Nivel}!");
+        }
+    }
+}
---
+++ MMOCS/MMOSOLUCAO/MMOCS/Program.cs
@@ -0,0 +1,150 @@
+﻿
+
+using MMOCS;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Mago Bia = new Mago();
+        Bia.Nome = "Beatrice";
+        Bia.Nivel = 2;
+        Bia.QI = 2;
+        Bia.Agilidade = 56;
+        Bia.Vida = 65;
+        Bia.Raca = "Humana";
+        Bia.Forca = 73;
+        Bia.Defesa = 40;
+
+        Cavaleiro At = new Cavaleiro();
+        At.Nome = "Atena";
+        At.Nivel = 2;
+        At.QI = 30;
+        At.Agilidade = 53;
+        At.Vida = 76;
+        At.Raca = "Humana";
+        At.Forca = 82;
+        At.Defesa = 37;
+
+        Anao An = new Anao();
+        An.Nome = "Jiorge";
+        An.Nivel = 2;
+        An.QI = 19;
+        An.Agilidade = 24;
+        An.Vida = 92;
+        An.Raca = "Anao";
+        An.Forca = 69;
+        An.Defesa = 87;
+
+        Elfo El = new Elfo();
+        El.Nome = "Jambo";
+        El.Nivel = 4;
+        El.QI = 87;
+        El.Agilidade = 69;
+        El.Vida = 54;
+        El.Raca = "Elfo";
+        El.Forca = 27;
+        El.Defesa = 31;
+
+        Console.WriteLine("Bem vindos a Maden Fight! a arena onde sai apenas um campeão");
+        Console.WriteLine("Selecione seu jogador:");
+        Console.WriteLine("1 - Cavaleiro\n2 - Mago\n3 - Elfo\n4 - Anão");
+        int escolhaJogador = int.Parse(Console.ReadLine());
+        Personagem jogador = null;
+        switch (escolhaJogador)
+        {
+            case 1:
+                jogador = At;
+                break;
+            case 2:
+                jogador = Bia;
+                break;
+            case 3:
+                jogador = El;
+                break;
+            case 4:
+                jogador = An;
+                break;
+            default:
+                Console.WriteLine("Faça uma escolha válida!");
+                break;
+        }
+        jogador.apresentar();
+        Console.WriteLine("---------|#|---------");
+        Console.WriteLine("Selecione seu inimigo:");
+        Console.WriteLine("1 - Cavaleiro\n2 - Mago\n3 - Elfo\n4 - Anão");
+        int escolhaInimigo = int.Parse(Console.ReadLine());
+        Personagem inimigo = null;
+        switch (escolhaInimigo)
+        {
+            case 1:
+                inimigo = At;
+                break;
+            case 2:
+                inimigo = Bia;
+                break;
+            case 3:
+                inimigo = El;
+                break;
+            case 4:
+                inimigo = An;
+                break;
+            default:
+                Console.WriteLine("Faça uma escolha válida!");
+                break;
+        }
+        inimigo.apresentar();
+        if (escolhaJogador == escolhaInimigo)
+        {
+            Console.WriteLine("Você não pode lutar contra si mesmo!");
+        }
+        Console.WriteLine("---------|#|---------");
+        Console.WriteLine($"A luta ficou entre {jogador.Nome} o(a) {jogador.Raca} e {inimigo.Nome} o(a) {inimigo.Raca}");
+
+        while (true)
+        {
+            Console.WriteLine("Selecione a ação:");
+            Console.WriteLine("1 - Usar Poção de Cura\n2 - Atacar\n3 - Subir de Nivel\n4 - Sair");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+
+            if (escolha == 1)
+            {
+                jogador.regenerarVida();
+                if (inimigo.Vida <= 0)
+                {
+                    Console.WriteLine("Você derrotou seu Inimigo");
+                    break;
+                }
+            }
+            else if (escolha == 2)
+            {
+                jogador.atacar(inimigo);
+                Console.WriteLine($"Seu inimigo ficou com:{inimigo.Vida} de vida!");
+
+                if (inimigo.Vida <= 0)
+                {
+                    Console.WriteLine("Você derrotou seu Inimigo");
+                    break;
+                }
+            }
+            else if (escolha == 3)
+            {
+                jogador.subirNivel();
+            }
+            else if (escolha == 4)
+            {
+                break;
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
